f388bba25c4f44e200e07aa8b443b534e905a5f5
inkgab <keithar123@gmail.com>
2013-12-26 13:53:25 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Techoweb::Application.config.secret_key_base = '6cb96695e75a3b9dad73526632d62da4188e2fd5f4626d5a67ed93408f99323a646017b872a93601a81b41662ea09199bf29adc01d685dbc667bb855b946bca4'
